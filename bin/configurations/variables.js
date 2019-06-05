2784211c7296e82333110b735262a56d0e83c99e
israelgoomes <rael_goomes@hotmail.com>
2019-06-05 19:18:00 -0300

    ----------

--- bin/configurations/variables.js
+++ bin/configurations/variables.js
@@ -3,9 +3,10 @@ const variables = {
     port: process.env.port || 3000
   },
   Database: {
+    //conectando ao mongoDB
     connection:
       process.env.connection ||
-      "mongodb+srv://israel:88096672@cluster0-xzat1.mongodb.net/novadesign"
+      "mongodb+srv://user:password@cluster0-xzat1.mongodb.net/nomeDoBanco"
   },
   Security: {
     secretKey: "364e569fdc48b433c95dda58009d3737"
